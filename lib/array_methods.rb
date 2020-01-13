f50f252d61ed3d2037fabce82eeb7ba3648cfad9
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-13 01:06:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,9 +16,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  x = 0
+  x = [0]
   array.length.times {|index|
-    x = array[index]
     if array[index] > x
       x = array[index]
     end
