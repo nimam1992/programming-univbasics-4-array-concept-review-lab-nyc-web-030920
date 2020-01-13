5e97770154845ff82e73de195de7c0c624289042
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-13 01:02:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,11 +16,17 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  x = 0
+  array.length.times{|index|
+    x = array(index)
+    if array[index] > x 
+      x = array[index]
+    end
+  }
+  return x
 end
 
 def find_min_value(array)
   # Add your solution here
 end
-letters = ['A','B','C']
-find_element_index(letters,'A')
+
